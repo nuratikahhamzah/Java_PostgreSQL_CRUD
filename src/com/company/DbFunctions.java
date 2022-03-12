bbb26ab846aadab099d8993b42cf39074113a318
Nur Atikah <49825016+nuratikahhamzah@users.noreply.github.com>
2022-03-12 14:54:50 +0800

    Update

--- src/com/company/DbFunctions.java
+++ src/com/company/DbFunctions.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DbFunctions {
@@ -40,6 +41,23 @@ public class DbFunctions {
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Row Inserted!");
+        }catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void read_data(Connection conn, String table_name) {
+        Statement statement;
+        ResultSet rs = null;
+        try{
+            String query = String.format("SELECT * FROM %s", table_name);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("empID")+" ");
+                System.out.print(rs.getString("name")+" ");
+                System.out.println(rs.getString("address")+" ");
+            }
         }catch (Exception e) {
             System.out.println(e);
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,9 @@ public class Main {
         DbFunctions db = new DbFunctions();
         Connection conn = db.connect_to_db("testDB", "postgres", "123456");
 //        db.createTable(conn, "Employee");
-        db.insert_row(conn, "Employee", "Hannah", "Singapore");
-        db.insert_row(conn, "Employee", "Daniel", "Indonesia");
-        db.insert_row(conn, "Employee", "Lily", "Thailand");
+//        db.insert_row(conn, "Employee", "Hannah", "Singapore");
+//        db.insert_row(conn, "Employee", "Daniel", "Indonesia");
+//        db.insert_row(conn, "Employee", "Lily", "Thailand");
+        db.read_data(conn, "Employee");
     }
 }
